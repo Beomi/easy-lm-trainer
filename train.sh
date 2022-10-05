9d3b3055b35b584521d35bc2daa6435367816054
Junbum Lee <jun@beomi.net>
2022-10-05 11:35:01 +0900

    Update train arguments
    - 기존 KoGPT 예시 대신 Eleuther Polyglot 3.8B를 예시로 사용
    - train, test 파일 인자 변경

--- train.sh
+++ train.sh
@@ -1,11 +1,21 @@
+# Kakao KoGPT 6B 사용 예시
+# python main.py \
+#     --model_name='kakaobrain/kogpt' \
+#     --model_revision='KoGPT6B-ryan1.5b-float16' \
+#     --bos_token='[BOS]' \
+#     --eos_token='[EOS]' \
+#     --unk_token='[UNK]' \
+#     --pad_token='[PAD]' \
+#     --mask_token='[MASK]' \
+#     --train_file_path='./data/train.csv' \
+#     --test_file_path='./data/test.csv' \
+#     --num_train_epochs=10 \
+#     --data_text_column='text'
+
+# Ployglot-Ko 3.8B 사용 예시
 python main.py \
-    --model_name='kakaobrain/kogpt' \
-    --model_revision='KoGPT6B-ryan1.5b-float16' \
-    --bos_token='[BOS]' \
-    --eos_token='[EOS]' \
-    --unk_token='[UNK]' \
-    --pad_token='[PAD]' \
-    --mask_token='[MASK]' \
-    --data_file_path='./data/data.csv' \
-    --num_train_epochs=10 \
-    --data_text_column='lm_text'
+    --model_name='EleutherAI/polyglot-ko-3.8b' \
+    --train_file_path='./data/train.csv' \
+    --test_file_path='./data/test.csv' \
+    --num_train_epochs=5 \
+    --data_text_column='text'
